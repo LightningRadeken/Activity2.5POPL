c2b8186986913925c0ed13c2afe4ae84326b0272
Malachi Mooty <radeken2015@gmail.com>
2026-04-01 11:17:06 -0600

    Update Worksheet2.5Stephens.py
    Add my changes

--- Worksheet2.5Stephens.py
+++ Worksheet2.5Stephens.py
@@ -3,6 +3,24 @@
 from functools import wraps
 import time
 
+#Decorator 3
+def validation_decorator(func):
+    @wraps(func)
+    def wrapper(x, y):
+        if isinstance(x, int) and isinstance(y,int):
+            return func(x,y)
+        else:
+            print("Invalid inputs. Please provide integers.")
+            return None
+    return wrapper
+
+#@validation_decorator
+#def add_nums(x,y):
+#    return x + y
+
+#print(add_num(3,4))
+#print(add _num(5, "a"))
+
 
 def timing_decorator(func):
     @wraps(func)
